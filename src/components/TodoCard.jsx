bbbff169f1630d3e20c32f3af1a7ac28ba6daee8
17BTCS023 <post.mk02@gmail.com>
2024-09-21 17:54:39 +0530

    fixed module script not found

--- src/components/TodoCard.jsx
+++ src/components/TodoCard.jsx
@@ -1,7 +1,7 @@
-import React from "react";
+import React from 'react'
 
 export default function TodoCard(props) {
-    const { handleDeleteTodo,handleEditTodo, children, index} = props;
+    const { handleDeleteTodo,handleEditTodo, children, index} = props
     return (
         <li className='todoItem'>
                 {children}
